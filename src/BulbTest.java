4a464e213a2c5b5335679dccab4986337893af5e
sanya <rajanisanya@gmail.com>
2017-05-10 11:17:00 +0530

    4th commit with value 200

--- src/BulbTest.java
+++ src/BulbTest.java
@@ -10,13 +10,13 @@ public class BulbTest {
 	@Before
 	public void setUp() throws Exception 
 	{
-		bulb = new Bulb(50);
+		bulb = new Bulb(200);
 	}
 
 	@Test
 	public void test() 
 	{
-		assertEquals(50,bulb.getBulbWatts());
+		assertEquals(200,bulb.getBulbWatts());
 	}
 
 }
